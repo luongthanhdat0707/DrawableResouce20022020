4672f859560f31cc7e9ba0c9f1bd7f4a73ce5fd6
luongthanhdat0707 <luongthanhdat0707>
2020-05-04 22:23:13 +0700

    checkbox

--- app/src/main/java/com/example/drawableresource25022020/MainActivity.java
+++ app/src/main/java/com/example/drawableresource25022020/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.drawableresource25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox mcbandroid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mcbandroid = findViewById(R.id.checkbox);
+        mcbandroid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(MainActivity.this, "Da check", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Bo check", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
